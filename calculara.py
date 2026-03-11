5c171a1c2e85fc01e7bf3e71a993bdfa8a448620
Baruk_Morelli <baruk.pamponeti@escola.pr.gov.br>
2026-03-11 09:56:51 -0300

    Add files via upload

---
+++ calculara.py
@@ -0,0 +1,27 @@
+#calculadora de aprovação escolar
+
+input("Digite o nome do estudante: ")
+
+soma_notas = 0
+quantidade_trimestre = 3
+meta_aprovação = 180
+
+for i in range(1, quantidade_trimestre = 3
++ 1):
+nota= float(inpute("informe a Nota{i}º"))
+soma_notas += nota
+
+print("-" * 30)
+print(f"pontuação total:") {soma_notas}
+")
+
+#verificação o status
+if soma_notas >= meta_aprovação
+print("status:aprovadão! parabens!! Finalmente!!")
+else:
+    pontos_faltantes + meta_aprovação + soma_notas
+    print("status: TENTE OUTRA VEZ!!")
+    print(f"faltaram{pontos_faltantes}") pontod para atingir o mínimo de {meta_aprovação}.")
+     print(f"Status: Desaprovado | Você precissa de {pontos_faltantes} pontos para passar.")
+
+
